7cf09c57ce1e6dee56056ff123ef69514d77eb80
AbulFozolJumman <abulfozoljumman@gmail.com>
2025-06-27 11:39:41 +0600

    Initial setup done

---
+++ src/lib/auth.ts
@@ -0,0 +1,50 @@
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectDB } from "./mongoose";
+import User from "@/models/user.model";
+import bcrypt from "bcryptjs";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        await connectDB();
+        const user = await User.findOne({ email: credentials?.email });
+        if (!user) throw new Error("No user found");
+
+        const isValid = await bcrypt.compare(
+          credentials!.password!,
+          user.password
+        );
+        if (!isValid) throw new Error("Incorrect password");
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/login",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    async session({ session, token }) {
+      if (token && session.user) session.user.role = token.role;
+      return session;
+    },
+  },
+};
